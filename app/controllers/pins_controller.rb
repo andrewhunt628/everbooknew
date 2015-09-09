ed8c66ed2411fd78db34d76dae065b5950ba806f
Andrew Hunt <andrewhunt@andrewhunt-Aspire-ES1-511.(none)>
2015-09-08 21:35:25 -0500

    Justin Fix

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
 	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_filter :authenticate_user!
 
 	def index
 		@pins = Pin.all.order("created_at DESC")
