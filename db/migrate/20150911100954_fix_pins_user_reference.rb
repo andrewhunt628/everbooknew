3a05e354f777600ab66d2eea57d4510957e45fd1
Justin Harrison <justin@matthin.com>
2015-09-11 06:14:13 -0400

    Fix pins user reference

---
+++ db/migrate/20150911100954_fix_pins_user_reference.rb
@@ -0,0 +1,5 @@
+class FixPinsUserReference < ActiveRecord::Migration
+  def change
+    rename_column :pins, :userid, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908021323) do
+ActiveRecord::Schema.define(version: 20150911100954) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "userid"
+    t.integer  "user_id"
   end
 
-  add_index "pins", ["userid"], name: "index_pins_on_userid"
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
