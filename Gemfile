86e2d906d37a6165c0913c3a02165e74f545529e
Andrew Hunt <andrewhunt@andrewhunt-Aspire-ES1-511.(none)>
2015-10-28 20:52:13 -0500

    Photo upload is now working

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'haml'
 gem 'bootstrap-sass', '~> 3.3.5'
 gem 'simple_form'
 gem 'devise'
+gem "paperclip", "~> 4.3"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,6 @@ Rails.application.configure do
   # for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
+  #for paperclip
+  Paperclip.options[:command_path] = "/usr/bin/convert/"
 end
