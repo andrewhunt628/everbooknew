5953f44b913a6fd8b2302728385ed852fe35c304
Alexey Volobuev <17valexl@gmail.com>
2016-02-09 12:01:18 +0300

    Add specs for family_bonds_controller. Make some fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,36 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryGirl.build(:user) }
+  let(:father) { FactoryGirl.create(:user) }
+  let(:child)  { FactoryGirl.create(:user) }    
 
   it 'successful saved when build from FactoryGirl' do
     expect(user.save!).to eq(true)
   end
 
+
+  context "not_from_family" do
+    before(:each) do
+      father.save!
+      child.save!
+      user.save!
+    end
+    
+    it "doesn't return given user" do
+      expect(User.not_from_family(user).any?{|u| u == user}).to eq(false)
+    end
+
+    it "doesn't return family" do
+      father.add_family_member(child)
+      expect(User.not_from_family(father).any?{|u| u == child}).to eq(false)
+    end
+
+    it "returns user which is not a member of family" do
+      expect(User.not_from_family(father).any?{|u| u == user}).to eq(true)
+    end
+  end
+
   context "add_family_member" do
-    let(:father) { FactoryGirl.create(:user) }
-    let(:child)  { FactoryGirl.create(:user) }    
 
     it 'create new Family record' do
       expect do
