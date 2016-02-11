1d272fe7229918497e012415cfb83d3557492f05
Alexey Volobuev <17valexl@gmail.com>
2016-02-11 09:19:22 +0300

    Move registration logic to landing page. Add jquery form validation for form. Make some fixes

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,8 @@ module Everbooknew
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.to_prepare do
-      Devise::RegistrationsController.layout false
-      Devise::SessionsController.layout false
+      Devise::RegistrationsController.layout 'landing'
+      Devise::SessionsController.layout 'landing'
     end
   end
 end
