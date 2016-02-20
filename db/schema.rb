60010472f949b0cfb20d2733b975af9dfe8439d9
Alexey Volobuev <17valexl@gmail.com>
2016-02-20 19:53:24 +0300

    Remove fake data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220122204) do
+ActiveRecord::Schema.define(version: 20160220165005) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
