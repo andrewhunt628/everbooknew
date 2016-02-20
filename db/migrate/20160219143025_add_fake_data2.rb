8e25725a33183341a4ba9f87882c1ef003721232
Alexey Volobuev <17valexl@gmail.com>
2016-02-20 19:01:26 +0300

    Fix fake data

--- db/migrate/20160219143025_add_fake_data2.rb
+++ db/migrate/20160219143025_add_fake_data2.rb
@@ -1,5 +1,5 @@
 class AddFakeData2 < ActiveRecord::Migration
-  def change
+  def up
     return unless Rails.env.development?
     Album.destroy_all
     Pin.destroy_all
@@ -7,12 +7,24 @@ class AddFakeData2 < ActiveRecord::Migration
     users.each do |user|
       folder_path = "#{Rails.root.to_s}/public/BenPhoto"
       5.times.each do 
-        album = Album.create! title: Forgery('lorem_ipsum').title, description: Forgery('lorem_ipsum').paragraph, user: user
+        album = Album.create! title: Forgery('lorem_ipsum').title, description: Forgery('lorem_ipsum').paragraph, user: user, tag_list: get_random_tags
         8.times do |i|
           album.pins.create! title: "#{i+1}-2.jpg", image: File.open("#{folder_path}/#{i+1}-2.jpg", 'rb'), description: Forgery('lorem_ipsum').paragraph
         end
       end
     end
+  end
 
+  def down
+    
+  end
+
+  def get_random_tags
+    tags = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth']
+    result = []
+    5.times do 
+      result.push(tags[rand(10)])
+    end
+    result.uniq.join(",")
   end
 end
