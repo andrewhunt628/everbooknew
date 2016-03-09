3cc572ec12a9b5f21a9d508314a2e82dd76b5989
opan <opan.neutron@gmail.com>
2016-03-09 21:07:38 +0700

    add spec test api_key model

--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :api_key do
-    
+    expired_date Date.today+1
+    association :user
+  end
+
+  factory :api_expired, parent: :api_key do
+    expired_date Date.yesterday
   end
 
 end
