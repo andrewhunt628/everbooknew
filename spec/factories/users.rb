89e71f64ba192dc29977e55d18e04e36bda3e28c
opan <opan.neutron@gmail.com>
2016-03-09 22:16:40 +0700

    add spec for sessions_controller api

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,9 @@ FactoryGirl.define do
     password_confirmation '11223344'
   end
 
+  factory :user_fail_login, parent: :user do
+    password "123124"
+    password_confirmation "12319879"
+  end
+
 end
