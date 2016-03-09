09221f8903992487bc16175d6e4db9b16fe92e87
opan <opan.neutron@gmail.com>
2016-03-09 20:27:52 +0700

    update and fix spec test comment model

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,16 +1,15 @@
 FactoryGirl.define do
   factory :album do
     title { Forgery('name').industry }
-
-    # trait :pins do
-      # association :pin, factory: :pin, image: File.new(Rails.root + 'spec/fixtures/images/example1.jpg')
-    # end
   end
 
-  factory :valid_album, parent: :album do |f|
-    f.after(:build) do |album|
-      p album
+  factory :valid_album, parent: :album do
+    after(:build) do |album|
       album.pins.build({image: File.new(Rails.root + 'spec/fixtures/images/example1.jpg')})
+    end
+
+    after(:create) do |album|
+      album.pins.create({image: File.new(Rails.root + 'spec/fixtures/images/example1.jpg')})
     end
   end
 
--- spec/factories/pins.rb
+++ spec/factories/pins.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :pin do     
-    association :album, factory: :album
+    association :album, factory: :valid_album
     after(:build) do |pin|
       pin.image = File.new(Rails.root + 'spec/fixtures/images/example1.jpg')
 
+    end
+
+    after(:create) do |pin|
+      pin.image = File.new(Rails.root + 'spec/fixtures/images/example1.jpg')
     end
   end
 
