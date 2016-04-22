87b57517ab1c836ab1570761d5c42eec13eca187
Mark Frost <cheerful.mf@gmail.com>
2016-04-23 01:45:28 +0500

    remove incorrect migration
    trying to remove ‘friendships’, but the table does not exist

--- db/migrate/20160411015745_drop_friendships.rb
+++
@@ -1,8 +0,0 @@
-class DropFriendships < ActiveRecord::Migration
-   #this migration is getting rid of an old friendships table that i made by 
-   #mistake. how can i get rid of it?
-
-   def change
-      drop_table :friendships
-   end
-end
