4837395a022b2957af51f9f8d660d6eeedeab3f9
james <penakamaayo@gmail.com>
2016-04-24 20:39:38 +0800

    Setup tempoary friendrequest email template

--- app/mailers/friendship_notifications.rb
+++ app/mailers/friendship_notifications.rb
@@ -1,17 +1,11 @@
 class FriendshipNotifications < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.friendship_notifications.new_invitation.subject
-  #
-  def new_invitation(from, to)
-    
-    @from = from
-    @to = to
+  def new_invitation inviter, invitee
 
-    @greeting = "Hi"
+    @inviter = inviter
+    @invitee = invitee
 
-    mail to: to.email, subject: 'You have a new friend request on Kidio'
+    mail :to => @invitee.email,
+         :subject => 'You have a new friend request on Kidio'
   end
 end
