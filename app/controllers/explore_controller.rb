a4ef4d6bda57d4d32db6e9f0504c981298916c33
james <penakamaayo@gmail.com>
2016-04-24 20:56:37 +0800

    Display message when tags or friends list is empty

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,12 +1,7 @@
 class ExploreController < ApplicationController
 
   def index
-    @albums = Album.where(user_id: current_user.family_ids + [current_user.id])
-                .order('albums.created_at DESC')
-
-    @pins = @albums.reduce([]) {|n, album| album.pins + n}
-                .sort_by!(&:updated_at).reverse!
-    @tags = @albums.tag_counts_on(:tags)
+    @tags = current_user.owned_tags.alphabetical
 
     @users = User.all_except(current_user).decorate
   end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,2 @@
 class TagsController < ApplicationController
-
-  def index
-    @tags = current_user.owned_tags.alphabetical
-  end
-
 end
