ee89a5e85e5e2fcaa4b104490fdeaa11f82404d9
james <penakamaayo@gmail.com>
2016-04-29 12:56:37 +0800

    Setup mailtrapio for staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,7 @@
+require 'rubygems' if RUBY_VERSION < '1.9'
+require 'rest_client'
+require 'json'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -102,4 +106,19 @@ Rails.application.configure do
   #     }
   # }
 
+
+
+  response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+
+  first_inbox = JSON.parse(response)[0] # get first inbox
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => first_inbox['username'],
+    :password => first_inbox['password'],
+    :address => first_inbox['domain'],
+    :domain => first_inbox['domain'],
+    :port => first_inbox['smtp_ports'][0],
+    :authentication => :plain
+  }
 end
