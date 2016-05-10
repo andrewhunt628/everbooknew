591f4c700b804186e133cfb6b5d51deeec60da66
james <penakamaayo@gmail.com>
2016-05-10 10:58:30 +0800

    Add location to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430012757) do
+ActiveRecord::Schema.define(version: 20160510025650) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 20160430012757) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
